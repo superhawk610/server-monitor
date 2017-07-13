9889742856d9df0d4b125372605c9d9c3f9524c3
superhawk610 <superhawk610@gmail.com>
2017-07-13 16:40:45 -0400

    Device location recognition

---
+++ client/models/Device.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var DeviceSchema = new Schema({
+  name: String,
+  ip: String,
+  mac: String,
+  port: String,
+  make: String,
+  model: String,
+  icon: String,
+  tags: [{
+    name: String,
+    category: String
+  }],
+  description: {
+    tagline: String,
+    short: String,
+    full: String
+  },
+  credentials: [{
+    user: String,
+    pass: String
+  }]
+})
+
+module.exports = mongoose.model('Device', DeviceSchema)
